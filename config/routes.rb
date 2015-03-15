aecea00d3c8e4016c3f1046e7149b721c75258b2
Michael Scriven <bigscriv03@gmail.com>
2015-03-15 16:35:53 -0400

    Added doctors controller, views, routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :doctors, only: [:index, :show, :edit]
   resources :countries, only: [:index, :show]
 
   # The priority is based upon order of creation: first created -> highest priority.
